b0a82120412bfab9d9c687a9e9c7a4e3accbf7da
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2020-02-17 22:37:23 +0900

    Reusing the backdrop for sp

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,26 @@ import './SideDrawer.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+  let attachedClassed = ["SideDrawer", "Close"];
+  if (props.open) {
+    attachedClassed = ["SideDrawer", "Open"]
+  }
   return (
-    <div className="SideDrawer">
-      <div className="LogoSide">
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClassed.join(' ')}>
+        <div className="LogoSide">
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
